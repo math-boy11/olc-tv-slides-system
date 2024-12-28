b7df07234ac94e6488b169ba5a416fd1f64510f0
Theo <theo.boersma046@gmail.com>
2024-12-27 19:03:34 -0800

    Finished basic slideshow rendering

--- script.js
+++ script.js
@@ -1,84 +1,56 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var slideChangeInterval = 3000;
+var slideContainer = $("#slide-container");
+var slide = $("#slide");
+var slideChangeInterval = 5000;
 var currentSlideIndex = 0;
-var presentationData;
+var data;
 
-//Get the Google Slides data from the server
-google.script.run.withSuccessHandler(function (response) {
-    //Hide the loading text and show the canvas
-    document.getElementById("loading").style.display = "none";
-    canvas.style.display = "initial";
+//Get the Google Slides pdf from the server
+$.ajax({
+    url: "https://script.google.com/macros/s/AKfycbzGiG4PXzJbdYSFdbK0OyTA6L1CUH-Dyhd6Iw_rlF_B2vAiCBUu5Ip2v43gy4P1aKkC-w/exec",
+    type: "POST",
+    dataType: "json"
+}).done(function (slideshowData) {
+    //Hide the loading text and show the slide container
+    $("#loading").css("display", "none");
+    slideContainer.css("display", "initial");
 
-    presentationData = response;
+    //Assign the data as a global variable
+    data = slideshowData;
 
-    //Adjust the canvas dimensions to fill the screen
-    resizeCanvas();
+    //Adjust the slide dimensions to fill the screen
+    resize();
     drawSlide();
 
-    window.addEventListener("resize", function () {
-        resizeCanvas();
-        drawSlide();
-    });
+    $(window).resize(resize);
 
     //Interval to change the slide
     setInterval(function () {
         //Loop back to the first slide if we are currently on the last slide
-        if (presentationData.slides.length - 1 == currentSlideIndex) {
+        if (data.slides.length - 1 == currentSlideIndex) {
             currentSlideIndex = 0;
         } else {
             currentSlideIndex++;
         }
 
         drawSlide();
-    }, slideChangeInterval)
-}).getGoogleSlidesData();
+    }, slideChangeInterval);
+});
 
-//Function to draw the current slide onto the canvas
+//Function to draw the current slide onto the DOM
 function drawSlide() {
-    var slide = presentationData.slides[currentSlideIndex];
-    console.log(slide);
-
-    //Paint white background as the bottom layer
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    //Draw the slide background      
-    if (slide.background.type == "COLOR") {
-        ctx.fillStyle = slide.background.color;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    } else {
-        var backgroundImage = new Image();
-        backgroundImage.src = slide.background.imageURL;
-
-        backgroundImage.onload = function () {
-            ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-        }
-    }
-
-    //Draw the slide elements
-    for (var elem in slide.elements) {
-        if (elem.type == "IMAGE") {
-            var image = new Image();
-            image.src = elem.imageURL;
-
-            image.onload = function () {
-                ctx.drawImage(image, elem.x, elem.y, elem.width, elem.height);
-            }
-        }
-    }
+    slide.attr("src", data.slides[currentSlideIndex]);
 }
 
 //Function to resize the canvas to fit the screen
-function resizeCanvas() {
+function resize() {
     //Get the window dimensions
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
 
     //Calculate the scale factor based on the window size and aspect ratio
-    var scale = Math.min(windowWidth / presentationData.pageWidth, windowHeight / presentationData.pageHeight);
+    var scale = Math.min(windowWidth / data.width, windowHeight / data.height);
 
     //Set the canvas width and height based on the scale
-    canvas.width = presentationData.pageWidth * scale;
-    canvas.height = presentationData.pageHeight * scale;
+    slide.width(data.width * scale);
+    slide.height(data.height * scale);
 }
